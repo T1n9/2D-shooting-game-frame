f962221208aa01ef71d3f3d74a6ca46e952ced11
futinn@gmail.com <futinn@gmail.com>
2018-12-09 16:02:38 +0100

    fix keyboard bug

--- src/com/game/main/KeyInput.java
+++ src/com/game/main/KeyInput.java
@@ -2,9 +2,11 @@ package com.game.main;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.nio.file.FileAlreadyExistsException;
 
 public class KeyInput extends KeyAdapter {
     private Handler handler;
+    private boolean[] key_down = {false, false, false, false};
 
     public KeyInput(Handler handler){
         this.handler = handler;
@@ -14,10 +16,10 @@ public class KeyInput extends KeyAdapter {
         int k = e.getKeyCode();
         for(GameObject object : handler.objects){
             if(object.getId() == ID.Player){
-                if(k == KeyEvent.VK_W)  object.setVelY(-1 * Player.speed);
-                else if(k == KeyEvent.VK_A)  object.setVelX(-1 * Player.speed);
-                else if(k == KeyEvent.VK_S)  object.setVelY(Player.speed);
-                else if(k == KeyEvent.VK_D)  object.setVelX(Player.speed);
+                if(k == KeyEvent.VK_W)  {   object.setVelY(-1 * Player.speed); key_down[0] = true;   }
+                else if(k == KeyEvent.VK_A)  {  object.setVelX(-1 * Player.speed); key_down[1] = true;   }
+                else if(k == KeyEvent.VK_S)  {  object.setVelY(Player.speed);   key_down[2] = true; }
+                else if(k == KeyEvent.VK_D)  {  object.setVelX(Player.speed);   key_down[3] = true; }
             }
         }
         if(k == KeyEvent.VK_ESCAPE) System.exit(1);
@@ -26,10 +28,14 @@ public class KeyInput extends KeyAdapter {
         int k = e.getKeyCode();
         for(GameObject object : handler.objects){
             if(object.getId() == ID.Player){
-                if(k == KeyEvent.VK_W)  object.setVelY(0);
-                else if(k == KeyEvent.VK_A)  object.setVelX(0);
-                else if(k == KeyEvent.VK_S)  object.setVelY(0);
-                else if(k == KeyEvent.VK_D)  object.setVelX(0);
+                if(k == KeyEvent.VK_W)  key_down[0] = false;
+                else if(k == KeyEvent.VK_A)  key_down[1] = false;
+                else if(k == KeyEvent.VK_S)  key_down[2] = false;
+                else if(k == KeyEvent.VK_D)  key_down[3] = false;
+
+                if(!key_down[0] && !key_down[2])    object.setVelY(0);
+                if(!key_down[1] && !key_down[3])    object.setVelX(0);
+
             }
         }
     }
